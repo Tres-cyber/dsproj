58663e6162e0ede68f10f39be3ff30de96b40431
JehhB <jwy.bayod@gmail.com>
2022-10-08 00:38:28 +0800

    Added logic for hamburger menu

--- src/components/nav-bar/nav-bar.tsx
+++ src/components/nav-bar/nav-bar.tsx
@@ -1,17 +1,23 @@
-import { FunctionComponent } from "react";
+import { useState, FunctionComponent } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import "./nav-bar.css";
 
 export const NavBar: FunctionComponent<{}> = function () {
+  const [active, setActive] = useState<boolean>(false);
+  const toggleActive = () => {
+    setActive((a) => !a);
+  };
+
   return (
     <nav className="nav-bar container">
       <a href="#" className="nav-bar__logo">
         oli
       </a>
       <FontAwesomeIcon
-      className="nav-bar__menu"
-      icon={faBars}
+        className={"nav-bar__menu" + (active ? "--active" : "")}
+        onClick={toggleActive}
+        icon={faBars}
       />
       <ul className="nav-bar__links">
         <li>
